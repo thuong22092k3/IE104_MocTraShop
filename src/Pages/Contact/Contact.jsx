27e2cd30a13525d0943461ac185c3e5448ce97b3
thuong22092k3 <everything22092k3@gmail.com>
2023-10-12 16:21:05 +0700

    Add breadcrumb for page Contact

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import Helmet from "../../Components/Helmet";
+import { Link} from "react-router-dom";
+import { Breadcrumb} from "antd";
+import BannerCate_Tra from"../../assets/Images/BannerCate_Tra.jpg";
 import Section, { SectionBody, SectionTitle } from "../../Components/Section";
 import Contact_Content from '../../Components/Contact/Contact_Content';
 const Contact = () => {
   return (
     <Helmet title="Trang chủ">
+       <div className="collections_header">
+            <img src={BannerCate_Tra} alt="" />
+            <div className="breadcrumb">
+                <Breadcrumb>
+                    <Breadcrumb.Item>
+                        <Link to="/">TRANG CHỦ</Link>
+                    </Breadcrumb.Item>
+                    <Breadcrumb.Item>LIÊN HỆ</Breadcrumb.Item>
+                </Breadcrumb>
+            </div>
+        </div>
       <Section>
         <SectionBody>
           <Contact_Content></Contact_Content>
