c4bf8d191e56a4cab869f29bcb2e652d0849e338
ngtueminh <nguyentueminh0305@gmail.com>
2023-10-14 09:34:41 +0700

    Fixed 'Contact' Page

--- src/Components/Contact/Contact_Content.jsx
+++ src/Components/Contact/Contact_Content.jsx
@@ -5,36 +5,37 @@ const Contact_Content = () => {
         <div className="contact">
             <div class="contact__content__header">
                 <h1>Hãy Liên Hệ Với Chúng Tôi</h1>
-                <p>Để biết thêm thông tin về sản phẩm và dịch vụ của chúng tôi. Xin vui lòng gửi <br /> email cho chúng tôi. Nhân viên của chúng tôi luôn có mặt để giúp đỡ bạn!</p>
+                <br />
+                <p class="grey_text">Để biết thêm thông tin về sản phẩm và dịch vụ của chúng tôi. Xin vui lòng gửi <br /> email cho chúng tôi. Nhân viên của chúng tôi luôn có mặt để giúp đỡ bạn!</p>
             </div>
             <div class="contact__content">
 
                 <div class="contact__content__main">
-                    <div class="contact__content__left">
-                        <div>
-                            <h2>Địa chỉ</h2>
+                    <div class="contact__content__main__left">
+                        <div class="contact__content__main__left__block">
+                            <p class="contact__content__main__left__text1">Địa chỉ</p>
                             <p>KTX Khu A, Đường số 6, <br /> Phường Linh Trung, Thủ Đức</p>
                         </div>
-                        <div>
-                            <h2>Số điện thoại</h2>
-                            <p>Di động: <br />
-                                Đường dây nóng:
+                        <div class="contact__content__main__left__block">
+                            <p class="contact__content__main__left__text1">Số điện thoại</p>
+                            <p>Di động: 0354466237 <br />
+                                Đường dây nóng: 0399776527
                             </p>
                         </div>
-                        <div>
-                            <h2>Thời gian làm việc</h2>
+                        <div class="contact__content__main__left__block">
+                            <p class="contact__content__main__left__text1">Thời gian làm việc</p>
                             <p>Thứ2-Thứ6: 8:00-22:00 <br />
                                 Thứ7-Chủ Nhật: 8:00-21:00
                             </p>
                         </div>
                     </div>
-                    <div class="contact__content__right">
+                    <div class="contact__content__main__right">
                         <p>Tên của bạn</p>
-                        <input type="text" placeholder='Nguyễn Văn A' />
+                        <input type="text" placeholder='Abc' />
                         <p>Địa chỉ gmail</p>
                         <input type="text" placeholder='Abc@gmail.com' />
                         <p>Chủ đề</p>
-                        <input type="text" />
+                        <input type="text" placeholder='Abc'/>
                         <p>Bạn có câu hỏi gì về chủ đề này?</p>
                         <textarea name="" id="" cols="30" rows="10" placeholder='Abc...'></textarea> <br />
                         <div class="button">
