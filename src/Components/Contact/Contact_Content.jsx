1ba064da57b7dd4704e43d94039c727ebc756324
ngtueminh <nguyentueminh0305@gmail.com>
2023-10-14 10:12:06 +0700

    Add icon in 'Contact' Page

--- src/Components/Contact/Contact_Content.jsx
+++ src/Components/Contact/Contact_Content.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { LocationIcon, PhoneIcon, CupIcon } from '../Icons'
 const Contact_Content = () => {
     return (
         <div className="contact">
@@ -13,17 +13,20 @@ const Contact_Content = () => {
                 <div class="contact__content__main">
                     <div class="contact__content__main__left">
                         <div class="contact__content__main__left__block">
-                            <p class="contact__content__main__left__text1">Địa chỉ</p>
+
+                            <p class="contact__content__main__left__text1"><LocationIcon /> Địa chỉ</p>
                             <p>KTX Khu A, Đường số 6, <br /> Phường Linh Trung, Thủ Đức</p>
                         </div>
                         <div class="contact__content__main__left__block">
-                            <p class="contact__content__main__left__text1">Số điện thoại</p>
+
+                            <p class="contact__content__main__left__text1"><PhoneIcon /> Số điện thoại</p>
                             <p>Di động: 0354466237 <br />
                                 Đường dây nóng: 0399776527
                             </p>
                         </div>
                         <div class="contact__content__main__left__block">
-                            <p class="contact__content__main__left__text1">Thời gian làm việc</p>
+
+                            <p class="contact__content__main__left__text1"><CupIcon /> Thời gian làm việc</p>
                             <p>Thứ2-Thứ6: 8:00-22:00 <br />
                                 Thứ7-Chủ Nhật: 8:00-21:00
                             </p>
@@ -35,10 +38,10 @@ const Contact_Content = () => {
                         <p>Địa chỉ gmail</p>
                         <input type="text" placeholder='Abc@gmail.com' />
                         <p>Chủ đề</p>
-                        <input type="text" placeholder='Abc'/>
+                        <input type="text" placeholder='Abc' />
                         <p>Bạn có câu hỏi gì về chủ đề này?</p>
                         <textarea name="" id="" cols="30" rows="10" placeholder='Abc...'></textarea> <br />
-                        <div class="button">
+                        <div class="button_gui">
                             <button id="button_Gui">Gửi</button>
                         </div>
                     </div>
